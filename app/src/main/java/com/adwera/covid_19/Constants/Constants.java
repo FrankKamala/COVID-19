e1217c1950df88c8f2b781b42c0302bba0995e2a
FrankKamala <frankkamala@gmail.com>
2020-03-28 13:53:10 +0300

    add myClient and Api Interface

--- app/src/main/java/com/adwera/covid_19/Constants/Constants.java
+++ app/src/main/java/com/adwera/covid_19/Constants/Constants.java
@@ -1,6 +1,6 @@
 package com.adwera.covid_19.Constants;
 
 public class Constants {
-    public static final String BASE_URL = "https://blablabla";
+    public static final String BASE_URL_NEWS = "https://api.breakingapi.com/news";
     public static final String BASE_STATS_URL = "https://corona.lmao.ninja/";
 }
