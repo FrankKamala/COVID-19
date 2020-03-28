fb7a39a00764518983dd248a44540c27ac9b961b
ShadrackAdwera <adweshshaddie@gmail.com>
2020-03-28 11:03:06 +0300

    add pojo for country, api and the client

--- app/src/main/java/com/adwera/covid_19/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.adwera.covid_19;
-
-public class Constants {
-public static final String BASE_CORONA_URL = "https://corona.lmao.ninja/";
-}
--- app/src/main/java/com/adwera/covid_19/network/CoronaClient.java
+++ app/src/main/java/com/adwera/covid_19/network/CoronaClient.java
@@ -1,4 +1,38 @@
 package com.adwera.covid_19.network;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class CoronaClient {
+    private static Retrofit retrofit = null;
+
+    public static CoronaApi getClient() {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_CORONA_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(CoronaApi.class);
+    }
 }
